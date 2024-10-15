96d1a12eced2c039dbd43dbdaee938b1c4fa90fc
Shawn <sb810@hotmail.com>
2024-10-15 00:03:24 -0400

    massive website updates, mostly done now. temporarily removed about us page.

---
+++ public/js/firebase_func.js
@@ -0,0 +1,4 @@
+// Initialize Firebase
+
+//import * as firebase from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+
--- public/js/font-preloader.js
+++ public/js/font-preloader.js
@@ -1,3 +1,20 @@
+let fontsLoaded = false;
+let windowLoaded = false;
+
 document.fonts.ready.then(function() {
-	document.getElementsByClassName("page-spinner-wrapper")[0].style = 'opacity:0; pointer-events:none;';
+  if(!windowLoaded)
+    fontsLoaded = true;
+  else showPage();
 });
+
+window.onload = function() {
+  if(!fontsLoaded)
+    windowLoaded = true;
+  else showPage();
+}
+
+function showPage() {
+  setTimeout(() => {
+    document.getElementsByClassName("page-spinner-wrapper")[0].style = 'opacity:0; pointer-events:none;';
+  }, 500);
+}
